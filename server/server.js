9016e2074a4258fd2a68380cc3127adabdb638f3
Carlos Asencio Morales <carlosgkt@gmail.com>
2024-06-12 22:03:16 -0500

    Updating server.js for deployment on renders

--- server/server.js
+++ server/server.js
@@ -32,11 +32,11 @@ async function startApolloServer() {
 
     if (process.env.NODE_ENV === 'production') {
       app.use(express.static(path.join(__dirname, '../client/build')));
-    }
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/build/index.html'));
-    });
+      app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+      });
+    }
 
     db.once('open', () => {
       app.listen(PORT, () => {
